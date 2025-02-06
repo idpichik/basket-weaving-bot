92c0f053f8b1f1eba4270fad612bdaf7645ec77b
idpichik <d.ioann910@gmail.com>
2025-02-06 20:20:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Вставь сюда свой токен, который ты получил от BotFather
+# Вставь сюда свой токен
 TOKEN = '7880774464:AAGBEe1pYDmT-NzWvVgKJBfyrCfj7mLSu8A'
 
 # Функция, которая будет вызываться при команде /start
@@ -11,11 +11,7 @@ def start(update: Update, context: CallbackContext) -> None:
 # Основная функция, которая запускает бота
 def main():
     # Создаем объект Updater с твоим токеном
-<<<<<<< HEAD
     updater = Updater(TOKEN)
-=======
->>>>>>> e141045f6e1121939fc845bb10adf8d37dfa67bb
-    updater = Updater('7880774464:AAGBEe1pYDmT-NzWvVgKJBfyrCfj7mLSu8A')
 
     # Получаем диспетчер, чтобы добавить обработчики команд
     dispatcher = updater.dispatcher
